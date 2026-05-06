9b551884ef1e7acc033c52baaead7c12d699a110
meowmeow8888 <mosseriguy8@gmail.com>
2026-05-06 20:13:26 +0300

    chore: cert creation script

---
+++ create_cert.py
@@ -0,0 +1,40 @@
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+import datetime
+
+# Generate key
+key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+
+# Build cert
+subject = issuer = x509.Name([
+    x509.NameAttribute(NameOID.COMMON_NAME, "localhost"),
+])
+
+cert = (
+    x509.CertificateBuilder()
+    .subject_name(subject)
+    .issuer_name(issuer)
+    .public_key(key.public_key())
+    .serial_number(x509.random_serial_number())
+    .not_valid_before(datetime.datetime.utcnow())
+    .not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=365))
+    .add_extension(
+        x509.SubjectAlternativeName([x509.DNSName("localhost")]),
+        critical=False,
+    )
+    .sign(key, hashes.SHA256())
+)
+
+# Save key
+with open("key.pem", "wb") as f:
+    f.write(key.private_bytes(
+        serialization.Encoding.PEM,
+        serialization.PrivateFormat.TraditionalOpenSSL,
+        serialization.NoEncryption()
+    ))
+
+# Save cert
+with open("cert.pem", "wb") as f:
+    f.write(cert.public_bytes(serialization.Encoding.PEM))
